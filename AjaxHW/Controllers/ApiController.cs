ea663c996f8b14fb5dbae139d09ccd74855fed21
ShawnShen <a655059@hotmail.com.tw>
2022-10-05 11:30:12 +0800

    123

---
+++ AjaxHW/Controllers/ApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using AjaxHW.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxHW.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+        public ApiController(DemoContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CheckName(string Name)
+        {
+            string rst = "1";
+            if(_context.Members.Where(i => i.Name == Name).FirstOrDefault() != null)
+            {
+                rst = "0";
+            }
+            return Content(rst, "text/plain", System.Text.Encoding.UTF8);
+        }
+    }
+}
